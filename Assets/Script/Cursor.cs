1ec6e4d4647cc0b334ba54e1c117309e545de208
EliasHaloun0104 <elias.haloun0104@stud.hkr.se>
2019-10-22 12:03:34 +0200

    Add Cursor manager class

---
+++ Assets/Script/Cursor.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UnityEngine;
+
+namespace Script
+{
+    
+    public class Cursor : MonoBehaviour
+    {
+        [SerializeField] private GameObject handle;
+        [SerializeField] private float a = 0.0075f;
+        [SerializeField] private float b = 0.2525f;
+        [SerializeField] private float c = -4f;
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            var position = handle.transform.position;
+            var x = position.x;
+            var y = (float) (a * Math.Pow(x,2) + b * x + c);
+            //y = Mathf.Clamp(y, -13, 13);
+            transform.position = new Vector3(x,y,0);
+        }
+    }
+}
