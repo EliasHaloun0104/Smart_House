f4387039a4175e3f25d92e75b95896462cbec149
Nuno Cunha <33864862+iamnuno@users.noreply.github.com>
2019-11-11 10:21:46 +0100

    HTTP methods and JAR file

---
+++ Assets/Script/Device.cs
@@ -0,0 +1,41 @@
+namespace Client
+{
+    public class Device
+    {
+        private string deviceId, deviceName, deviceStatus;
+        
+        public Device() {}
+
+        public Device(string deviceId, string deviceName, string deviceStatus)
+        {
+            this.deviceId = deviceId;
+            this.deviceName = deviceName;
+            this.deviceStatus = deviceStatus;
+        }
+
+        public string DeviceId
+        {
+            get => deviceId;
+            set => deviceId = value;
+        }
+
+        public string DeviceName
+        {
+            get => deviceName;
+            set => deviceName = value;
+        }
+
+        public string DeviceStatus
+        {
+            get => deviceStatus;
+            set => deviceStatus = value;
+        }
+
+        public override string ToString()
+        {
+            return "Device Id: " + deviceId + "\n" +
+                   "Device Name: " + deviceName + "\n" +
+                   "Device Status " + deviceStatus + "\n";
+        }
+    }
+}
